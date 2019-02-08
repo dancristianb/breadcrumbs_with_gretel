fbe13bda21c18ef2da299ff82fe9d0a422d05f81
Dan Bota <dan.bota@unifiedpost.com>
2019-02-08 16:10:09 +0200

    add seeds using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do
+  Album.create(title: Faker::Lorem.words(2).join(' ').titleize,
+               description: Faker::Lorem.paragraph(3, true, 4),
+               artist: Faker::Name.name,
+               price: Faker::Commerce.price)
+end
